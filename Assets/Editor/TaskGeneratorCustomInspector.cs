9bd04a8db9eb1553281e45188355146fa854bd6d
pooky-programs <serafina.myers.7@gmail.com>
2025-11-03 09:52:46 -0600

    added new task generator

--- Assets/Editor/TaskGeneratorCustomInspector.cs
+++ Assets/Editor/TaskGeneratorCustomInspector.cs
@@ -2,26 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
 
 [CustomEditor(typeof(TaskGenerator))]
 public class TaskGeneratorCustomInspector : Editor
 {
 
     public string mapName;
+    [SerializeField] private TextAsset file;
     public override void OnInspectorGUI() {
         base.OnInspectorGUI();
         EditorGUILayout.LabelField("Taskset Generator");
         if (GUILayout.Button("Generate Tasklist")) {
-            // generate shit into txt files that importTasks can understand
-            mapName = EditorGUILayout.TextField("Target Map Name", mapName);
+            string assetPath = AssetDatabase.GetAssetPath(file);
+            string fullPath = Path.GetFullPath(assetPath);
 
-            // if the peeked does not match target map name then 
+            mapName = EditorGUILayout.TextField("Target Map Name", mapName);
+            if (file == null) {
+                Debug.LogError("Enter a valid task file (.txt)");
+            }
+            List<string> lines = new List<string>();
+            lines.AddRange(file.text.Split("\n"));
+
+            string fileMapName = lines[1];
+            if (fileMapName != mapName) {
+                Debug.LogError("The map name found in this file does not exist.");
+            }
+            string newContent = "";
 
             // get all waypoint transforms
             // random times
             // random task priority
-            // estimatedDuration (literaly idk)
-            // loadingTime 5 idk
+
+            File.WriteAllText(fullPath, newContent);
+            AssetDatabase.Refresh();
 
         }
         if (GUILayout.Button("stupid button")) {
